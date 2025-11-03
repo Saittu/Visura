860c78dcfa85f022ee3551eab922db1bced5ec79
Saito <jeanants@icloud.com>
2025-11-02 22:38:49 -0300

    feat: Ajustes realizados na tela de login e de registros. Iniciadas as configurações da api.

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -2,14 +2,27 @@
 
 import { Input } from '../../components/ui/input/page'
 import { Button } from '@/app/components/ui/button/page'
+import { Checkbox } from '@/app/components/ui/checkbox/page'
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger
+} from '@/app/components/ui/hoverCard/page'
+import { Label } from '@/app/components/ui/label/page'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 export default function LoginPage() {
   const router = useRouter()
+  const [LoginPassword, setLoginPassword] = useState(false)
+
+  function handleCheckboxChange() {
+    setLoginPassword(!LoginPassword)
+  }
 
   return (
     <div className='flex h-screen items-center justify-center'>
-      <main className='dark:bg-card h-120 w-160 rounded-lg border-2'>
+      <main className='dark:bg-card h-auto w-160 rounded-lg border-2'>
         <div>
           <div className='mb-1 flex items-center justify-center gap-2 pt-6'>
             <img src='/logo.svg' alt='teste' className='h-7 w-8' />
@@ -25,11 +38,39 @@ export default function LoginPage() {
               placeholder='Email'
               className='dark:bg-background'
             />
-            <Input
-              type='password'
-              placeholder='Senha'
-              className='dark:bg-background mt-4'
-            />
+            {!LoginPassword && (
+              <Input
+                type='password'
+                placeholder='Senha'
+                className='dark:bg-background mt-4'
+              />
+            )}
+
+            <div className='mt-6 ml-1 flex items-center'>
+              <Checkbox id='TypeLogin' onCheckedChange={handleCheckboxChange} />
+              <Label htmlFor='TypeLogin' className='ml-2 text-[14px]'>
+                Entrar sem senha.
+              </Label>
+              <HoverCard>
+                <HoverCardTrigger asChild>
+                  <Button
+                    variant='hoverCard'
+                    size='hc'
+                    className='relative ml-1'
+                  >
+                    ?
+                  </Button>
+                </HoverCardTrigger>
+                <HoverCardContent>
+                  <div>
+                    <p>
+                      No momento de entrada será encaminhado para seu e-mail um
+                      código de uso único para entrada.
+                    </p>
+                  </div>
+                </HoverCardContent>
+              </HoverCard>
+            </div>
           </div>
 
           <div className='mb-6 flex justify-center gap-5 px-13'>
@@ -47,7 +88,7 @@ export default function LoginPage() {
             <div className='bg-border h-0.5 w-5/6'></div>
           </div>
 
-          <div className='flex items-center justify-center gap-8'>
+          <div className='flex items-center justify-center gap-8 pb-8'>
             <div className='bg-secondary-foreground rounded-lg p-2'>
               <img src='/Google_logo.png' alt='' className='w-7' />
             </div>
